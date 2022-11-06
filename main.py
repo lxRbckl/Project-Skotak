aa00c218bb911a429762e9d0634b183a8bf6d4a2
ala2q6 <lxrbckl@protonmail.com>
2022-11-06 00:10:50 -0500

    beta version progress

--- main.py
+++ main.py
@@ -1,168 +1,217 @@
-# Project Fenaverat by Alex Arbuckle #
+# Project Skotak by Alex Arbuckle #
 
 
 # import <
-from os import path
-from json import loads
 from github import Github
 from asyncio import sleep
 from discord import Intents
 from discord.ext import commands
 from datetime import datetime as dt
-from lxRbckl import jsonLoad, jsonDump, githubSet, githubGet
-
-from requests import get
+from lxRbckl import githubSet, githubGet, requestsGet
 
 # >
 
 
+# token <
 # global <
-gMinute = 120
-gBranch = ''
-gFile = ''
-gRepository = ''
-gPath = path.realpath(__file__).split('/')
-gDirectory = '/'.join(gPath[:(len(gPath) - 1)])
 githubToken = ''
-skotak = commands.Bot(command_prefix = '', intents = Intents.all())
 discordToken = ''
 
+
+gFile = 'setting.json'
+gGithub = Github(githubToken)
+gRepository = 'lxRbckl/Project-Skotak'
+skotak = commands.Bot(command_prefix = '', intents = Intents.all())
+gDataLink = 'https://github.com/lxRbckl/Project-Skotak/raw/main/data.json'
+gSettingLink = 'https://github.com/lxRbckl/Project-Skotak/raw/main/setting.json'
+
 # >
 
 
-async def setFunction(ctx, pKey: str, pAction: str, pValue: str, pData: dict):
+async def setFunction(
+
+        ctx,
+        pKey: str = None,
+        pValue: str = None,
+        pElement: str = None,
+        pSetting: dict = None
+
+):
     '''  '''
 
-    # if (new value) then set value <
-    if (pValue not in pData[pKey][pAction]):
+    # set <
+    # update <
+    if (isinstance(pSetting[pKey], int)): pSetting[pKey] = int(pValue)
+    elif (isinstance(pSetting[pKey], list)): pSetting[pKey].append(pValue)
+    elif (isinstance(pSetting[pKey], dict)): pSetting[pKey][pValue].append(pElement)
 
-        pData[pKey][pAction].append(pValue)
-        jsonDump(
+    githubSet(
 
-            pData = pData,
-            pFile = f'{gDirectory}/setting.json'
+        pFile = gFile,
+        pData = pSetting,
+        pGithub = gGithub,
+        pRepository = gRepository
+
+    )
+
+    # >
+
+
+async def getFunction(
+
+        ctx,
+        pKey: str = None,
+        pValue: str = None,
+        pElement: str = None,
+        pSetting: dict = None
+
+):
+    '''  '''
+
+    # get <
+    # send <
+    if (not pKey): content = '\n'.join(pSetting.keys())
+    elif (isinstance(pSetting[pKey], int)): content = pSetting[pKey]
+    elif (isinstance(pSetting[pKey], list)): content = '\n'.join(pSetting[pKey])
+    elif (isinstance(pSetting[pKey], dict)): content = '\n'.join(pSetting[pKey][pValue])
+
+    await ctx.channel.send(
+
+        delete_after = 60,
+        content = f'`{content}`'
+
+    )
+
+    # >
+
+
+async def delFunction(
+
+        ctx,
+        pKey: str = None,
+        pValue: str = None,
+        pElement: str = None,
+        pSetting: dict = None
+
+):
+    '''  '''
+
+    # delete <
+    # update <
+    if (isinstance(pSetting[pKey], list)): pSetting[pKey].remove(pValue)
+    elif (isinstance(pSetting[pKey], dict)): pSetting[pKey][pValue].remove(pElement)
+
+    githubSet(
+
+        pData = pSetting,
+        pGithub = gGithub,
+        pRepository = gRepository
+
+    )
+
+    # >
+
+
+@skotak.command(aliases = requestsGet(pLink = gSettingLink)['aliases'])
+async def functionCommand(
+
+        ctx,
+        pKey: str = None,
+        pValue: str = None,
+        pElement: str = None
+
+):
+    '''  '''
+
+    await {
+
+        'set' : setFunction,
+        'get' : getFunction,
+        'del' : delFunction
+
+    }[ctx.invoked_with.lower()](
+
+        ctx = ctx,
+        pKey = pKey,
+        pValue = pValue,
+        pElement = pElement,
+        pSetting = githubGet(
+
+            pFile = gFile,
+            pGithub = gGithub,
+            pRepository = gRepository
 
         )
 
-    # >
-
-
-async def getFunction(ctx, pKey: str, pValue: str, pAction: str, pData: dict):
-    '''  '''
-
-    # if (return requested content) <
-    if (pAction): r = [f'`{i}`' for i in pData[pKey][pAction]]
-    elif (pKey): r = [f'`{i}`' for i in pData[pKey].keys()]
-    else: r = [f'`{i}`' for i in pData.keys()]
-
-    # >
-
-    await ctx.channel.send(delete_after = 60, content = '\n'.join(r))
-
-
-async def delFunction(ctx, pKey: str, pValue: str, pAction: str, pData: dict):
-    '''  '''
-
-    # if (existing value) then get value <
-    if (pValue in pData[pKey][pAction]):
-
-        pData[pKey][pAction].remove(pValue)
-        jsonDump(
-
-            pData = pData,
-            pFile = f'{gDirectory}/setting.json'
-
-        )
-
-    # >
+    )
 
 
 @skotak.event
-async def on_ready(pGithub = Github(githubToken)):
+async def on_ready():
     '''  '''
 
+    # set (aData) <
+    # while (running) <
     aData = {}
     while (True):
 
-        data = jsonLoad(pFile = f'{gDirectory}/setting.json')
-        bData = {'topic' : data['topic']['add'], 'language' : data['language']['add']}
-        for r in [r for u in data['user']['add'] for r in pGithub.get_user(u).get_repos()]:
+        # set (data, setting) <
+        # iterate (repository) <
+        setting = requestsGet(pLink = gSettingLink)
+        bData = {'topic' : setting['topic']['add'], 'language' : setting['language']['add']}
+        for r in [r for u in setting['user'] for r in gGithub.get_user(u).get_repos()]:
 
-            # if (permitted project) <
-            if (r not in data['project']['remove']):
+            # if (included project) <
+            if (r.full_name.split('/')[1] not in setting['project']['remove']):
 
-                feed = githubGet(
+                # get feed <
+                # add repository <
+                try:
 
-                    pGithub = pGithub,
-                    pFile = 'feed.json',
-                    pRepository = r.full_name
+                    feed = githubGet(
 
-                )
+                        pBranch = 'main',
+                        pGithub = gGithub,
+                        pFile = 'feed.json',
+                        pRepository = r.full_name
+
+                    )['feed']
+
+                except: feed = None
+
                 bData[r.full_name.split('/')[1]] = {
 
                     'projectLink' : f'https://github.com/{r.full_name}',
-                    'feedSubject' : list(feed.keys()) if (feed) else [],
                     'description' : r.description if (r.description) else 'None',
-                    'topic' : [t for t in r.get_topics() if (t not in data['topic']['remove'])],
-                    'feedLink' : f'https://raw.githubusercontent.com/{r.full_name}/main/feed.json',
-                    'language' : [l for l in r.get_languages() if (l not in data['language']['remove'])],
-                    'update' : dt.strptime(str(r.pushed_at).split(' ')[0], '%Y-%m-%d').strftime('%B %d %Y')
+                    'feedSubject' : list(feed['content'].keys()) if (feed) else [],
+                    'topic' : [t for t in r.get_topics() if (t not in setting['topic']['remove'])],
+                    'update' : dt.strptime(str(r.pushed_at).split(' ')[0], '%Y-%m-%d').strftime('%B %d %Y'),
+                    'language' : [l for l in r.get_languages().keys() if (l not in setting['language']['remove'])],
+                    'feedLink' : f'https://raw.githubusercontent.com/{r.full_name}/main/feed.json' if (feed) else 'None'
 
                 }
 
+                # >
+
             # >
 
-        # if (update) <
+        # >
+
+        # if (change) <
+        # sleep <
         if (aData != bData):
 
             githubSet(
 
-                pFile = gFile,
                 pData = bData,
-                pGithub = pGithub,
-                pBranch = gBranch,
+                pGithub = gGithub,
+                pFile = 'data.json',
                 pRepository = gRepository,
-                pMessage = 'Skotak Automated Update'
 
             )
             aData = bData
 
-        # >
-
-        await sleep(60 * gMinute)
-
-
-@commands.has_permissions(administrator = True)
-@skotak.command(aliases = jsonLoad(pFile = f'{gDirectory}/setting.json')['aliases'])
-async def commandFunction(ctx, pKey: str = None, pAction: str = None, pValue: str = None):
-    '''  '''
-
-    # local <
-    isAction = True if (pAction in ['add', 'remove']) else False
-    data = jsonLoad(pFile = f'{gDirectory}/setting.json')
-    isKey = True if (pKey in data.keys()) else False
-
-    # >
-
-    # if ((existing key) and (existing action)) <
-    if ((isKey and isAction) or (ctx.invoked_with.lower() == 'get')):
-
-        # call function <
-        await {
-
-            'set' : setFunction,
-            'get' : getFunction,
-            'del' : delFunction
-
-        }[ctx.invoked_with.lower()](
-
-            ctx = ctx,
-            pKey = pKey,
-            pValue = pValue,
-            pAction = pAction,
-            pData = jsonLoad(pFile = f'{gDirectory}/setting.json')
-
-        )
+        await sleep(60 * setting['interval'])
 
         # >
 
